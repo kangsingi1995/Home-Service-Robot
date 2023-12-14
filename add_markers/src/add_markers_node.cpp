730ba320aedea88ee8d58799f0e1ac10e9052a96
kangsingi1995 <vothanhkiet3195@gmail.com>
2023-12-14 10:08:56 +0700

    Update add_markers_node.cpp

--- add_markers/src/add_markers_node.cpp
+++ add_markers/src/add_markers_node.cpp
@@ -8,36 +8,41 @@ double dropOffPos[2] = {-2.6, -4.86}; // the last point robot drop off
 
 double pose[2] = {0, 0};  // current pose
 
+enum State {
+    PICKUP,  // going to pick up zon
+    CARRY,   // carry to drop zone
+    DROP,    // already drop
+  } state = PICKUP;
+
 void get_current_pose(const nav_msgs::Odometry::ConstPtr& msg)
 {
   pose[0] = msg->pose.pose.position.x;
   pose[1] = msg->pose.pose.position.y;
 }
 
-double distToCurrentPos(double goalPos[2])
+// Function to calculate Manhattan Distance between two points
+double manhattanDistance(double x1, double y1, double x2, double y2)
 {
-  double dx = goalPos[0] - pose[0];
-  double dy = goalPos[1] - pose[1];
-  return sqrt(dx*dx + dy*dy);
+  return std::abs(x1 - x2) + std::abs(y1 - y2);
 }
 
-/* /brief: Calcute distance of robot with pick up zone
-* /detail return true if the distance of robot with pick-up zone lower than 0.2
-*         Otherwise is false
-*/
-bool reach_pick_up()
+bool CheckState_OfRobot()
 {
-  return distToCurrentPos(pickUpPos) < 0.2;
+	State stateRobot;
+	
+	if(state == PICKUP && manhattanDistance(pose[0], pose[1], pickUpPos[0], pickUpPos[1])< 0.2)
+	{
+		stateRobot = CARRY;
+	}
+	else if(state == CARRY && manhattanDistance(pose[0], pose[1], dropOffPos[0], dropOffPos[1])< 0.2)
+	{
+		stateRobot = DROP;
+	}
+	else
+		stateRobot = PICKUP;
+	return stateRobot;
 }
 
-/* /brief: Calcute distance of robot with drop-out zone
-* /detail return true if the distance of robot with drop-out zone lower than 0.2
-*         Otherwise is false
-*/
-bool reach_drop_zone()
-{
-  return distToCurrentPos(dropOffPos) < 0.2;
-}
 
 int main( int argc, char** argv )
 {
@@ -49,12 +54,7 @@ int main( int argc, char** argv )
   ros::Subscriber pose_sub = n.subscribe("odom", 10, get_current_pose);
 
   uint32_t shape = visualization_msgs::Marker::CUBE;
-
-  enum State {
-    PICKUP,  // going to pick up zon
-    CARRY,   // carry to drop zone
-    DROP,    // already drop
-  } state = PICKUP;
+  state = PICKUP;
 
   ROS_INFO("Going to pick up zone ... ");
   while (ros::ok())
@@ -91,38 +91,34 @@ int main( int argc, char** argv )
     marker.lifetime = ros::Duration();
 
     ros::spinOnce();
-
-    // Check if state equal pickup, add new maker with position set by user
+	
+	switch (state)
+    {
+	// Check state equal pickup, add new maker with position set by user
     // Check if robot reach maker, waiting 5s and change state to carry
-    if (state == PICKUP) {
-      marker.action = visualization_msgs::Marker::ADD;
-      marker.pose.position.x = pickUpPos[0];
-      marker.pose.position.y = pickUpPos[1];
-      marker_pub.publish(marker);
-      if (reach_pick_up()) {
-        sleep(5);
-        ROS_INFO("Carrying to drop zone ... ");
-        state = CARRY;
-      }
-    }
-    // Check if state equal carry, remove the first maker
+      case PICKUP:
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.pose.position.x = pickUpPos[0];
+        marker.pose.position.y = pickUpPos[1];
+        marker_pub.publish(marker);
+		state = CheckState_OfRobot();
+        break;
+    // Check state equal carry, remove the first maker
     // after that check robot reach drop zone or not, if yes change state to drop
-    else if (state == CARRY) {
-      marker.action = visualization_msgs::Marker::DELETE;
-      marker.pose.position.x = dropOffPos[0];
-      marker.pose.position.y = dropOffPos[1];
-      marker_pub.publish(marker);
-      if (reach_drop_zone()) {
-        ROS_INFO("Reached drop zone. ");
-        state = DROP;
-      }
-    }
-    // Otherwise, add new maker to visualize to make sure the maker is drop
-    else /* state == DROP */ {
-      marker.action = visualization_msgs::Marker::ADD;
-      marker.pose.position.x = dropOffPos[0];
-      marker.pose.position.y = dropOffPos[1];
-      marker_pub.publish(marker);
+      case CARRY:
+        marker.action = visualization_msgs::Marker::DELETE;
+        marker.pose.position.x = dropOffPos[0];
+        marker.pose.position.y = dropOffPos[1];
+        marker_pub.publish(marker);
+        state = CheckState_OfRobot();
+        break;
+	  // add new maker to visualize to make sure the maker is drop
+      case DROP: 
+        marker.action = visualization_msgs::Marker::ADD;
+        marker.pose.position.x = dropOffPos[0];
+        marker.pose.position.y = dropOffPos[1];
+        marker_pub.publish(marker);
+        break;
     }
   }
 }
